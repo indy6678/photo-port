178792af7eb455416bacd351e43806fb759e89d7
Ed <105404937+indy6678@users.noreply.github.com>
2022-10-07 14:19:33 -0500

    completed about page setup

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import About from './components/About';
+import Nav from "./components/Nav";
 
 function App() {
   return (
     // html in react is JSX, JavaScript XML, syntax extension system to JS
     <div>
+      <Nav></Nav>
       <main>
-        <About>
-        </About>
+        <About></About>
       </main>
     </div>
   );
